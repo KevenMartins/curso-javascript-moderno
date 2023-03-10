df0a8351cc92918cfb0a99d4ffcbac812c9432df
KevenMartins <124926761+KevenMartins@users.noreply.github.com>
2023-03-10 11:22:45 +0000

    Strict mode

---
+++ aula02.js
@@ -0,0 +1,7 @@
+"use strict" //modo restrito - força o utilizador a criar um codigo mais limpo
+
+let nome = "Keven Martins"
+
+console.log("keven")
+console.log(nome)
+console.log("O nome é " + nome)
